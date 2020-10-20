eff7407a11513d3f89d8db5f6395971a675b424f
hantatsang <hantatsang@gmail.com>
2020-10-21 05:25:49 +1100

    refactor: remove unused files

--- src/components/ui/Table.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Table = styled.table`
-  border: none;
-`;
-
-export default Table;
--- src/components/ui/TableRow.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-const TableRow = styled.tr`
-`;
-
-export default TableRow;
