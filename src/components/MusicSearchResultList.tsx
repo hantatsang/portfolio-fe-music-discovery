bfd4d07fbc2d0d5336436817444ae66450d8f194
hantatsang <hantatsang@gmail.com>
2020-10-20 23:45:47 +1100

    feat(MusicSearchResultListItem): add play function

--- src/components/MusicSearchResultList.tsx
+++ src/components/MusicSearchResultList.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { SearchMusicItemPayload } from '../types/SearchMusicItemPayload';
 import MusicSearchResultListItem from './MusicSearchResultListItem';
 
@@ -6,10 +6,32 @@ type Props = {
   results: SearchMusicItemPayload[],
 }
 
+const audio = new Audio();
+
 function MusicSearchResultList({ results }: Props) {
+  const [playing, setPlaying] = useState<boolean>(false);
+  // const [audio, _] = useState<HTMLAudioElement>(new Audio());
+  const [currentAudioUrl, setCurrentAudioUrl] = useState<string>('');
+
+  const playPreview = (url: string) => {
+    if (currentAudioUrl === url && playing) {
+      audio.pause();
+      setPlaying(false);
+      return;
+    }
+
+    audio.pause();
+    audio.src = url;
+    audio.play();
+    setPlaying(true);
+    setCurrentAudioUrl(url);
+  };
+
   return <Fragment>
     {results.map((result) => <MusicSearchResultListItem
       key={result.id}
+      playPreview={playPreview}
+      playingAudioUrl={currentAudioUrl}
       result={result}
     />)}
   </Fragment>
--- src/components/MusicSearchResultListItem.tsx
+++ src/components/MusicSearchResultListItem.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { SearchMusicItemPayload } from '../types/SearchMusicItemPayload';
 
+library.add(faPlay, faPause);
 
-const Card = styled.div`
-  background: #ffffff;
-  border-radius: 15px;
-  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
-  transition: all 0.5s;
-
-  &:hover {
-    box-shadow: 0 25px 60px rgba(0, 0, 0, 0.2);
-  }
+const CardImageContainer = styled.div`
+  width: 100%;
+  height: 280px;
+  border-radius: 15px 15px 0 0;
+  overflow: hidden;
 `;
 
 type CardImageProps = {
@@ -28,22 +28,27 @@ const CardImage = styled.div<CardImageProps>`
   transition: all 0.5s;
 `;
 
-const CardImageContainer = styled.div`
-  width: 100%;
-  height: 280px;
-  border-radius: 15px 15px 0 0;
-  overflow: hidden;
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 25px;
+  background: #ffffff;
+  border-radius: 15px;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
+  transition: all 0.5s;
+
+  &:hover {
+    box-shadow: 0 25px 60px rgba(0, 0, 0, 0.2);
+  }
 
   &:hover ${CardImage} {
     transform: scale(1.1);
-
   }
 `;
 
 const CardContent = styled.div`
   z-index: 2;
-  padding: 20px;
-  text-align: center;
+  padding: 25px;
 `;
 
 const CardHeader = styled.div`
@@ -52,15 +57,61 @@ const CardHeader = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const CardAction = styled.div``;
+const CardAction = styled.div`
+  margin-top: auto; // position to bottom of card
+  padding-right: 25px;
+  text-align: right;
+`;
+
+const CardActionButton = styled.button`
+  box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+  border: none;
+  border-radius: 50%;
+  outline: none;
+  background: #ff5242;
+  color: #ffffff;
+  width: 60px;
+  height: 60px;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: all 0.2s ease-in;
+
+  &:hover {
+    box-shadow: 0px 6px 10px 0px rgba(0,0,0,0.14);
+    background: #ff3b29;
+  }
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+`;
 
 type Props = {
   result: SearchMusicItemPayload,
+  playingAudioUrl: string,
+  playPreview: Function,
 }
 
-function MusicSearchResultListItem({ result }: Props) {
+function MusicSearchResultListItem({
+  result,
+  playingAudioUrl,
+  playPreview,
+}: Props) {
+  const [playing, setPlaying] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (playingAudioUrl !== result.preview) {
+      setPlaying(false);
+    }
+  }, [playingAudioUrl, result.preview]);
+
+  const handleClickPlay = (url: string) => {
+    playPreview(url);
+    setPlaying(!playing);
+  }
+
   return <Card>
-    {/* <img src={result.album.cover_big} alt={result.title} /> */}
     <CardImageContainer>
       <CardImage image={result.album.cover_big} />
     </CardImageContainer>
@@ -68,8 +119,17 @@ function MusicSearchResultListItem({ result }: Props) {
       <CardHeader>
         {result.title_short}
       </CardHeader>
-      {result.artist.name}
+      <p>Album: {result.album.title}</p>
+      <p>Artist: {result.artist.name}</p>
     </CardContent>
+    <CardAction>
+      <CardActionButton onClick={() => handleClickPlay(result.preview)}>
+        {playing
+          ? <FontAwesomeIcon icon="pause" aria-label="pause" />
+          : <FontAwesomeIcon icon="play" aria-label="play" />
+        }
+      </CardActionButton>
+    </CardAction>
   </Card>
 }
 
