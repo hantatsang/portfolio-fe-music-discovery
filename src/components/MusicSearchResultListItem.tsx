d36a1c8f1ce7151452adcf6c1397be316a0fc481
hantatsang <hantatsang@gmail.com>
2020-10-22 23:02:54 +1100

    refactor: remove code comments

--- src/components/MusicSearchResultListItem.tsx
+++ src/components/MusicSearchResultListItem.tsx
@@ -11,63 +11,6 @@ import { Card, CardAction, CardContent, CardHeader, CardImage } from './ui/Card'
 
 library.add(faPlay, faPause);
 
-// const CardImageContainer = styled.div`
-//   width: 100%;
-//   height: 280px;
-//   border-radius: 15px 15px 0 0;
-//   overflow: hidden;
-// `;
-
-// type CardImageProps = {
-//   readonly image: string;
-// }
-
-// const CardImage = styled.div<CardImageProps>`
-//   width: 100%;
-//   height: 100%;
-//   background-image: url(${props => props.image});
-//   background-size: cover;
-//   background-position: center;
-//   background-color: #000000;
-//   transition: all 0.5s;
-// `;
-
-// const Card = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   padding-bottom: 25px;
-//   background: #ffffff;
-//   border-radius: 15px;
-//   box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
-//   transition: all 0.5s;
-
-//   &:hover {
-//     box-shadow: 0 25px 60px rgba(0, 0, 0, 0.2);
-//   }
-
-//   &:hover ${CardImage} {
-//     transform: scale(1.1);
-//   }
-// `;
-
-// const CardContent = styled.div`
-//   z-index: 2;
-//   padding: 25px;
-// `;
-
-// const CardHeader = styled.div`
-//   font-size: 1.5rem;
-//   font-weight: bold;
-//   margin-bottom: 1rem;
-//   text-align: center;
-// `;
-
-// const CardAction = styled.div`
-//   margin-top: auto; // position to bottom of card
-//   padding-right: 25px;
-//   text-align: right;
-// `;
-
 type Props = {
   result: SongPayload,
 }
